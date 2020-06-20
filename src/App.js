f5bb467075ff143d6d9a628be785e7815525b01b
mernxl <mernxl@gmail.com>
2020-06-20 07:11:28 +0100

    chore: import creatella-react-test files, setup code flow
    The server has to run on separate port on development, but when deployed (build), it is served by the json server statically

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { API_ENDPOINT } from './config/contants';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Products Grid</h1>
+
+        <p>Here you're sure to find a bargain on some of the finest ascii available to purchase. Be
+          sure to peruse our selection of ascii faces in an exciting range of sizes and prices.</p>
+
+        <p>But first, a word from our sponsors:</p>
+        <img className="ad" alt="ads" src={API_ENDPOINT + "/ads/?r=" + Math.floor(Math.random()*1000)}/>
       </header>
+
+      <section className="products">
+        ... products go here ...
+      </section>
     </div>
   );
 }
