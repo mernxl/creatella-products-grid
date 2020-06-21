f45ce8761b5af7175ccd80e565e568b864831ece
mernxl <mernxl@gmail.com>
2020-06-21 13:02:29 +0100

    feat(utils): add `AdsFactory` class

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,43 @@
-
 export const formatCurrency = (valueInCents: number) => {
-  return `$${(valueInCents / 100).toFixed(2)}`
+  return `$${(valueInCents / 100).toFixed(2)}`;
+};
+
+/**
+ * AdsFactory
+ *
+ * Form of a cache ads creator, for the same instance, we get same
+ * order of ads, this solve issues when rendering next elements of
+ * page content
+ */
+export class AdsFactory {
+  ads = [];
+  index = 0;
+
+  generateAd() {
+    return Math.floor(Math.random() * 1000);
+  }
+
+  reset() {
+    this.index = 0;
+  }
+
+  getNext() {
+    let ad = this.ads[ this.index ];
+    if (ad === undefined) {
+      let newAd = this.generateAd();
+      // no consecutive, rare so check once
+      if (ad === newAd) {
+        newAd = this.generateAd();
+      }
+      ad = newAd;
+      this.index += 1;
+      this.ads.push(newAd);
+    }
+
+    this.index += 1;
+    return ad;
+  }
 }
 
-export * from './date.utils'
-export * from './fetch.utils'
+export * from './date.utils';
+export * from './fetch.utils';
