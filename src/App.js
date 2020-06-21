220c833de8c100c94aa5e45464dd1a6520f53f80
mernxl <mernxl@gmail.com>
2020-06-21 14:30:28 +0100

    feat: add sort buttons to home screen
    - for sorting Products in grid

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import './App.css';
 import { ProductsGrid } from './components/ProductsGrid';
 import { API_ENDPOINT } from './config/contants';
 
 function App() {
+  const [sort, setSort] = useState('');
+
+  // prevent homeAd from changing when doing refresh of products
+  const { current: homeAd } = useRef(Math.floor(Math.random() * 1000));
+
   return (
     <div className="App">
       <header>
@@ -13,11 +18,24 @@ function App() {
           sure to peruse our selection of ascii faces in an exciting range of sizes and prices.</p>
 
         <p>But first, a word from our sponsors:</p>
-        <img className="ad" alt="ads" src={API_ENDPOINT + "/ads/?r=" + Math.floor(Math.random()*1000)}/>
+        <img className="ad" alt="ads"
+             src={API_ENDPOINT + "/ads/?r=" + homeAd}/>
       </header>
 
+      <div className="products-sort">
+        <span>Sort Products By: </span>
+        <div className="products-sort-btn-block">
+          {['id', 'size', 'price'].map((order) =>
+            <button onClick={() => setSort(order)}
+                    className={`btn ${order === sort ? 'active-sort' : ''}`}
+            >
+              {order}
+            </button>)}
+        </div>
+      </div>
+
       <section className="products">
-        <ProductsGrid />
+        <ProductsGrid sort={sort}/>
       </section>
     </div>
   );
